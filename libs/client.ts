0f9e86fff0811cac11d6094bc9fb65d1b93dadb8
itc-n25011 <n25011@std.it-college.ac.jp>
2026-02-12 12:47:43 +0900

    修正

--- libs/client.ts
+++ libs/client.ts
@@ -1,7 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  // 👈 ここを microCMS の管理画面（image_dd9d1f.png）の値に更新します
-  serviceDomain: 'orix-insight', 
-  apiKey: 'LbhNOEgUYAeulGGJiyR1iGYxBHPxDAQFn1Co', 
+  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN || '',
+  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY || '',
 });
